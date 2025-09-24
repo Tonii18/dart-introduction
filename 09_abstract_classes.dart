8c34e810d659887080cfdbe7d0d02c34cbf8cedb
Tonii18 <ajsanrosal@gmail.com>
2025-09-24 14:14:05 +0200

    added content

--- 09_abstract_classes.dart
+++ 09_abstract_classes.dart
@@ -1,7 +1,17 @@
 void main(){
   
+  final windPlant = WindPlant(initialEnergy: 100);
   
+  print('Wind: ${chargePhone(windPlant)}');
   
+}
+
+double chargePhone(EnergyPlant plant){
+  if(plant.energyLeft < 10){
+    throw Exception('Not enough energy');
+  }
+  
+  return plant.energyLeft - 10;
 }
 
 enum PlantType {nuclear, wind, water}
@@ -19,5 +29,19 @@ abstract class EnergyPlant{
   });
   
   void consumeEnergy(double amount);
+  
+}
+
+// Extends and Implements
+
+class WindPlant extends EnergyPlant{
+  
+  WindPlant({required double initialEnergy})
+    : super(energyLeft: initialEnergy, type: PlantType.wind);
+  
+  @override
+  void consumeEnergy(double amount){
+    energyLeft -= amount;
+  }
   
 }
